09a51d64ea308ebfbc3778ced612d93ee8a68045
lallerNeha <nehalaller@mansainfotech.com>
2018-09-17 22:32:40 +0530

    expt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myproject';
+  ckeConfig = {
+            height: 400,
+            language: "en",
+            allowedContent: true,
+            toolbar: [
+                { name: "clipboard", items: ["Cut", "Copy", "Paste", "PasteText", "PasteFromWord", "-", "Undo", "Redo"] },
+                { name: "links", items: ["Link", "Unlink", "Anchor"] },
+                { name: "insert", items: ["Image", "Table", "HorizontalRule", "SpecialChar", "Iframe", "imageExplorer"] }
+            ]
+        };
+
+    openImageExplorer= function($event: any) {
+        this.showFiles = true; // open pop-up window
+    }
+    onCloseImage= function() {
+       this.showFiles = false; // close the pop-up window
+   }
+
+   onAddImage= function() {
+       try
+       {
+           let link = this.ckeditor.instance.document.createElement("img");
+           link.setAttribute("alt", "Image");
+           link.setAttribute("src", "./Images/test.png");
+
+           this.ckeditor.instance.insertElement(link);
+       }
+       catch(error)
+       {
+           console.log((<Error>error).message);
+       }
+
+       this.showFiles = false; // close the pop-up window
+   }
+   onChange = function(event){
+     console.log(event);
+     this.content = event;
+   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { CKEditorModule } from 'ng2-ckeditor';
+import { SanitizeHtml, SanitizeResourceUrl, SanitizeScript, SanitizeStyle, SanitizeUrl } from 'ng2-sanitize';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CKEditorModule,
+    SanitizeHtml
   ],
   providers: [],
   bootstrap: [AppComponent]
